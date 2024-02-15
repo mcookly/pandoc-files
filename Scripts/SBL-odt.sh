262f263b830ab366bb9b4b07485e4d64a67ac53b
Max <github.1@mxck.onl>
2024-02-15 15:50:31 -0500

    Add CSL style

--- Scripts/SBL-odt.sh
+++ Scripts/SBL-odt.sh
@@ -10,6 +10,7 @@ echo "============================="
 
 pandoc -i $1 -o $2.odt \
   --bibliography $3 \
+  --csl Resources/Styles/SBL2_full_note.csl \
   --reference-doc Resources/Templates/SBL2.odt \
   --lua-filter Resources/Filters/SBL2.lua
 
