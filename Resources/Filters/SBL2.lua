7d7210e3fe2d9364578d9ca8b0911f6d52b807d6
Max <20271792+mcookly@users.noreply.github.com>
2024-03-08 13:03:55 -0500

    Format

--- Resources/Filters/SBL2.lua
+++ Resources/Filters/SBL2.lua
@@ -68,26 +68,26 @@ function Pandoc(p)
   ))
 
   -- Before adding the document, we need to adjust H6 to be an inline heading.
-  for i = #doc.blocks-1, 1, -1 do
+  for i = #doc.blocks - 1, 1, -1 do
     if doc.blocks[i].tag == 'Header' and doc.blocks[i].level == 6 then
       local inline_para = doc.blocks[i].content
 
       -- Add punctuation if needed.
-      if not inline_para[#inline_para].text:match([[[%.%:%;%?%!]$]]) then 
+      if not inline_para[#inline_para].text:match([[[%.%:%;%?%!]$]]) then
         inline_para[#inline_para] = pandoc.Str(inline_para[#inline_para].text .. '.')
       end
 
       -- Format with emphasis per CMOS 17 guidelines.
       inline_para = { pandoc.Emph(pandoc.Inlines(inline_para)) }
 
-      inline_para[#inline_para+1] = pandoc.Space()
+      inline_para[#inline_para + 1] = pandoc.Space()
 
       -- Append paragraph content
-      for j = 1, #doc.blocks[i+1].content do
-        inline_para[#inline_para+1] = doc.blocks[i+1].content[j]
+      for j = 1, #doc.blocks[i + 1].content do
+        inline_para[#inline_para + 1] = doc.blocks[i + 1].content[j]
       end
 
-      doc.blocks[i+1].content = inline_para
+      doc.blocks[i + 1].content = inline_para
 
       -- Remove H6.
       doc.blocks:remove(i)
@@ -99,13 +99,13 @@ function Pandoc(p)
   -- For papers with no citations, no bibliography editing is needed.
   if blocks[#blocks].identifier == 'refs' then
     -- This is the best way to a heading with the right style.
-    blocks:insert(#blocks,pandoc.Div(pandoc.Para('Bibliography'),{ ['custom-style'] = 'Bibliography Heading' }))
+    blocks:insert(#blocks, pandoc.Div(pandoc.Para('Bibliography'), { ['custom-style'] = 'Bibliography Heading' }))
 
     -- Adjust the style of the bibliography if it exists.
     local bib = {}
     for _, entry in ipairs(blocks[#blocks].content) do
       entry.attr = { ['custom-style'] = 'Bibliography' }
-      bib[#bib+1] = entry
+      bib[#bib + 1] = entry
     end
     blocks[#blocks].content = bib
   end
@@ -114,14 +114,13 @@ function Pandoc(p)
 end
 
 function Inlines(i)
-
-  for j = #i-1, 1, -1 do
+  for j = #i - 1, 1, -1 do
     -- Convert en-dashes to em-dashes per SBL/CMOS 17 style.
     if i[j].tag == 'Str' then
-      if i[j].text == '–' and i[j-1].tag == 'Space' and i[j+1].tag == 'Space' then
-        i[j].text = '—' 
-        i:remove(j+1)
-        i:remove(j-1)
+      if i[j].text == '–' and i[j - 1].tag == 'Space' and i[j + 1].tag == 'Space' then
+        i[j].text = '—'
+        i:remove(j + 1)
+        i:remove(j - 1)
       end
     end
   end
